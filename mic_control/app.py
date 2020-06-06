55fac47e862ad876c0bb7fba7c1bbfe71bbeae9e
Aly Sivji <alysivji@gmail.com>
2020-06-06 16:28:35 -0500

    use async-await

--- mic_control/app.py
+++ mic_control/app.py
@@ -1,35 +1,28 @@
-#!/Users/alysivji/siv-dev/playground/sandbox/javascript/streamdeck/venv/bin/python
+#!/Users/alysivji/siv-dev/projects/home-automation/mute-microphone-mac-streamdeck-plugin/venv/bin/python
 
 import argparse
+import asyncio
 import json
 import logging
-import os
 
-import websocket
+import websockets
 
-logging.basicConfig(filename="example.log", level=logging.DEBUG)
-
-
-def on_open(ws):
-    logging.info("Input Volume")
-    # register device inside of program
-    ws.send(json.dumps(registration_dict))
-    # get settings
+logging.basicConfig(filename="example.log", level=logging.INFO)
 
 
-def on_message(ws, raw_message):
-    items = json.loads(raw_message)
-    if items["event"] == "keyUp":
-        os.system('osascript -e "set volume input volume 0"')
-    logging.info(raw_message)
+async def handle_messages(websocket):
+    async for message in websocket:
+        msg_dict = json.loads(message)
+        logging.info(msg_dict)
+        # if msg_dict["event"] == "keyUp":
+        #     os.system('osascript -e "set volume input volume 0"')
 
 
-def on_error(ws, error):
-    logging.error(error)
-
-
-def on_close(ws):
-    logging.info("exited")
+async def connect_and_listen(registration_info: dict, port: int):
+    uri = f"ws://localhost:{port}"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps(registration_info))
+        await handle_messages(websocket)
 
 
 def parse_args():
@@ -42,15 +35,13 @@ def parse_args():
 
 
 if __name__ == "__main__":
+    logging.info("program started")
     args = parse_args()
-    registration_dict = {"event": args.event, "uuid": args.plugin_uuid}
-
-    ws = websocket.WebSocketApp(
-        "ws://localhost:" + args.port,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close,
-    )
-    ws.on_open = on_open
-    ws.run_forever()
-    logging.info("got all the way here")
+    logging.info("parsed arguments")
+    registration_info = {"event": args.event, "uuid": args.plugin_uuid}
+    logging.info(args.info)
+    loop = asyncio.get_event_loop()
+    # schedule function to periodically get value from macbook
+    loop.run_until_complete(connect_and_listen(registration_info, port=args.port))
+    loop.run_forever()
+    logging.info("exited")
