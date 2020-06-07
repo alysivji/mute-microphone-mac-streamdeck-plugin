b8f93a188570ec6e6cd07b8639c9485829eb409f
Aly Sivji <alysivji@gmail.com>
2020-06-07 11:28:10 -0500

    refactor into class

--- mic_control/app.py
+++ mic_control/app.py
@@ -32,34 +32,46 @@ async def update_input_volume_from_system():
         microphone.volume
 
 
-async def connect_and_listen(registration_info: dict, port: int):
-    uri = f"ws://localhost:{port}"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send(json.dumps(registration_info))
-        await handle_messages(websocket)
+class StreamDeckClient:
+    def __init__(self, port, event, plugin_uuid, info):
+        logging.info(info)  # what's in here that we should save?
+        self.plugin_uuid = plugin_uuid
+        self.uri = f"ws://localhost:{port}"
+        self.registration_info = {"event": event, "uuid": plugin_uuid}
+        self.messages_to_send = []
 
+    async def connect_and_listen(self):
+        async with websockets.connect(self.uri) as websocket:
+            await websocket.send(json.dumps(self.registration_info))
+            await self.handle_messages(websocket)
 
-async def handle_messages(websocket):
-    async for message in websocket:
-        msg_dict = json.loads(message)
-        logging.info(msg_dict)
-        ee.emit(msg_dict["event"])
+    async def handle_messages(self, websocket):
+        async for message in websocket:
+            msg_dict = json.loads(message)
+            logging.info(msg_dict)
+            ee.emit(msg_dict["event"])
 
 
 @ee.on('keyDown')
 async def key_down_handler():
     microphone.toggle_mute()
+    if microphone._volume == 0:
+        # send state 1
+        pass
+    else:
+        # send state 2
+        pass
 
 
 if __name__ == "__main__":
     logging.info("program started")
     args = parse_command_line_arguments()
     logging.info("parsed arguments")
-    registration_info = {"event": args.event, "uuid": args.plugin_uuid}
-    logging.info(args.info)
 
     loop = asyncio.get_event_loop()
     asyncio.run_coroutine_threadsafe(update_input_volume_from_system(), loop)
-    loop.run_until_complete(connect_and_listen(registration_info, port=args.port))
+    streamdeck = StreamDeckClient(args.port, args.event, args.plugin_uuid, args.info)
+    logging.info("here")
+    loop.run_until_complete(streamdeck.connect_and_listen())
 
     logging.info("exited")
